e374e58890c1cb0cdf401a516fa31e12c4d47bdf
almogza <elyharow93@hotmail.com>
2023-07-06 10:31:31 +0000

    Install CORS and cancel auto tests

--- helloworld/application.py
+++ helloworld/application.py
@@ -4,8 +4,10 @@ from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
 import requests
 import boto3
+from flask_cors import CORS
 
 application = Flask(__name__)
+CORS(application, resources={r"/*": {"origins": "*"}})
 
 @application.route('/', methods=['GET'])
 def get():

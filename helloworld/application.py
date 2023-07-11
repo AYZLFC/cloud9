68c2bbf46e3e98380bedb870e075a171648db547
almogza <elyharow93@hotmail.com>
2023-07-11 16:45:22 +0000

    Debugingsolution test  animalDetails route

--- helloworld/application.py
+++ helloworld/application.py
@@ -41,10 +41,9 @@ def upload_image():
 #This route for get the animal's details
 @application.route('/animal_details', methods=['GET','POST'])
 def get_animal_details():
-    data = request.data
-    #animal_details(bucket="savepics", data=data , region="us-east-1", table_name="animalTable") #we need to change the name of the image to somthing constant and to make sure to set this name on the React code as the name of the image when user add an image
+    details=animal_details(bucket="savepics", image_name=request.data , region="us-east-1", table_name="animalTable") #we need to change the name of the image to somthing constant and to make sure to set this name on the React code as the name of the image when user add an image
     
-    return Response(data, mimetype='application/json', status=200)
+    return Response(json.dumps(details), mimetype='application/json', status=200)
 
 # # #Gets all animals from dynmoDB
 # @application.route('/get_animalTable', methods=['GET'])
--- helloworld/bl.py
+++ helloworld/bl.py
@@ -19,11 +19,7 @@ def handle_request_data(file, bucket, region):
 
     
 # This function get the image, recognize the labels, compare with the animals table and get the animal details    
-def animal_details(bucket, data, region, table_name):
-    # convert the json to dictionary
-    data_dict = json.loads(data)
-    #image_name=data
-    image_name = data_dict.get('body')
+def animal_details(bucket, image_name, region, table_name):
     # get the image from S3 bucket
     image = get_image(bucket, image_name, region)
     img_data = image.get()['Body'].read() # Read the image
